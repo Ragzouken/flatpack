78653b0016794d00975ca10c9c9c7621bb983e14
mark <ragzouken@gmail.com>
2017-04-08 18:03:15 +0100

    aw yeah

--- Assets/TouchTransformTest.cs
+++ Assets/TouchTransformTest.cs
@@ -12,30 +12,47 @@ public class TouchTransformTest : MonoBehaviour
 {
     [SerializeField]
     private Transform touch1, touch2, testObject;
-
-    private bool started;
+    
+    private bool oneFinger;
+    private bool twoFinger;
     private Vector2 prevTouch1, prevTouch2;
     private float baseScale, baseAngle;
     private Vector2 basePosition;
 
     private void Update()
     {
-        if (Input.GetMouseButton(0))
+        Vector2 nextTouch1 = Vector2.zero;
+        Vector2 nextTouch2 = Vector2.zero;
+
+        if (Input.touchCount > 0)
         {
-            touch1.position = Input.mousePosition;
+            nextTouch1 = Input.GetTouch(0).position;
         }
 
-        if (Input.GetMouseButton(1))
+        if (Input.touchCount > 1)
         {
-            touch2.position = Input.mousePosition;
+            nextTouch2 = Input.GetTouch(1).position;
         }
 
-        Vector2 nextTouch1 = touch1.position;
-        Vector2 nextTouch2 = touch2.position;
-
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.touchCount == 1 && !oneFinger)
         {
-            started = true;
+            prevTouch1 = nextTouch1;
+            basePosition = testObject.position;
+
+            oneFinger = true;
+        }
+        else if (Input.touchCount == 1 && oneFinger)
+        {
+            testObject.position = basePosition - prevTouch1 + nextTouch1;
+        }
+        else
+        {
+            oneFinger = false;
+        }
+
+        if (Input.touchCount == 2 && !twoFinger)
+        {
+            twoFinger = true;
 
             prevTouch1 = nextTouch1;
             prevTouch2 = nextTouch2;
@@ -44,16 +61,11 @@ public class TouchTransformTest : MonoBehaviour
             baseAngle = testObject.localEulerAngles.z;
             basePosition = testObject.position;
         }
-
-        if (started)
+        else if (Input.touchCount == 2 && twoFinger)
         {
-            Vector2 O = basePosition;
-            Vector2 A = prevTouch1;
-            Vector2 B = prevTouch2;
-
-            Vector2 a = A - O;
-            Vector2 b = B - O;
-            Vector2 c = B - A;
+            Vector2 a = prevTouch1 - basePosition;
+            Vector2 b = prevTouch2 - basePosition;
+            Vector2 c = prevTouch2 - prevTouch1;
 
             float prevD = (prevTouch2 - prevTouch1).magnitude;
             float nextD = (nextTouch2 - nextTouch1).magnitude;
@@ -63,15 +75,8 @@ public class TouchTransformTest : MonoBehaviour
             float nextAngle = Angle(nextTouch2 - nextTouch1);
             float deltaAngle = Mathf.DeltaAngle(prevAngle, nextAngle);
 
-            float prevMagA = a.magnitude;
-            float nextMagA = prevMagA * scaleMult;
-
-            Debug.LogFormat("{0} / {1}", scaleMult, deltaAngle);
-
             Vector2 nexta = Rotate(a * scaleMult, deltaAngle);
             Vector2 nextO = nextTouch1 - nexta;
-
-            //Debug.LogFormat("{0} / {1}", a, nexta);
 
             testObject.localScale = Vector3.one * baseScale * scaleMult;
             testObject.localEulerAngles = Vector3.forward * (baseAngle + deltaAngle);
@@ -79,15 +84,7 @@ public class TouchTransformTest : MonoBehaviour
         }
         else
         {
-            Debug.LogFormat("{0} ({1} / {2})",
-                            Angle(Input.mousePosition - touch1.position),
-                            Input.mousePosition,
-                            touch1.position);
-        }
-
-        if (Input.GetKeyUp(KeyCode.Space))
-        {
-            started = false;
+            twoFinger = false;
         }
     }
 
