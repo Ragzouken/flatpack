2afd12297935ed31faa7f34119857bb1dbfb4c26
mark <ragzouken@gmail.com>
2017-06-20 20:22:40 +0100

    add credit screen

---
+++ Assets/Scripts/Utility/OnEnableTrigger.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Assertions;
+using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+
+using Random = UnityEngine.Random;
+using UnityEngine.Events;
+
+public class OnEnableTrigger : MonoBehaviour 
+{
+    [Serializable]
+    public class Event : UnityEvent { }
+
+    public Event onEnable;
+
+    private void OnEnable()
+    {
+        onEnable.Invoke();
+    }
+}
